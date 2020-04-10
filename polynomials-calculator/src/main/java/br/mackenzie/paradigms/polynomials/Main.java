61d83f005155854bdc4422ad011c9efe79b5efa9
dias <daniel.dddl.pdf@gmail.com>
2020-04-10 10:28:06 -0300

    adding declaration required by the professor

--- polynomials-calculator/src/main/java/br/mackenzie/paradigms/polynomials/Main.java
+++ polynomials-calculator/src/main/java/br/mackenzie/paradigms/polynomials/Main.java
@@ -2,6 +2,24 @@ package br.mackenzie.paradigms.polynomials;
 
 import java.util.Scanner;
 
+/**
+ * Entrega de trabalho - Calculadora de polinômios
+ *
+ * Nós,
+ *
+ * Bárbara Este Fernandez
+ *
+ * Daniel Dias de Lima, e
+ *
+ * Luan Rocha Damato,
+ *
+ * declaramos que
+ *
+ * todas as respostas são fruto de nosso próprio trabalho,
+ * não copiamos respostas de colegas externos à equipe,
+ * não disponibilizamos nossas respostas para colegas externos à equipe e
+ * não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
+ * */
 public class Main {
 
     public static void main(String[] args) {
